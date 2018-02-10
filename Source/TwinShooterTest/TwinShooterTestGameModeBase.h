ae3cae79d56acf6d6bf12d63a0cc6481f75bf8fe
NickJaNinja <ndf99551@uga.edu>
2018-02-10 16:29:40 -0500

    Initial commit

---
+++ Source/TwinShooterTest/TwinShooterTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TwinShooterTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWINSHOOTERTEST_API ATwinShooterTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
